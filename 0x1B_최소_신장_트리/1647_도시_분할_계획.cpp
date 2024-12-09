ff4006ed83076aeb125f2d903329ec18165bb6f7
jeongyunnim <jeongyun7521@gmail.com>
2024-12-09 17:07:50 +0900

    feat: BOJ@1647 도시 분할 계획

--- 0x1B_최소_신장_트리/1647_도시_분할_계획.cpp
+++ 0x1B_최소_신장_트리/1647_도시_분할_계획.cpp
@@ -4,8 +4,8 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-tuple<int, int, int> planets[1001 * 1001];
-int p[1001];
+int p[100001];
+tuple<int, int, int> village[1000001];
 
 int find(int t) {
     if (p[t] == -1) return t;
@@ -26,25 +26,29 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int N;
-    cin >> N;
-    int pi = 0;
-    for (int i = 1; i <= N; ++i) {
-        for (int j = 1; j <= N; ++j) {
-            int x;
-            cin >> x;
-            if (i < j)
-                planets[pi++] = {x, i, j};
-        }
+    int N, M;
+    cin >> N >> M;
+    int vi = 0;
+    for (int i = 1; i <= M; ++i) {
+        int x, u, v;
+        cin >> u >> v >> x;
+        village[vi++] = {x, u, v};
     }
     memset(p, -1, sizeof(p));
-    sort(planets, planets + pi);
-    int mn = 0;
-    for (int i = 0; i < pi; ++i) {
+    sort(village, village + vi);
+    int mn = 0, cnt = 0;
+    if (N < 3) {
+        cout << 0;
+        return 0;
+    }
+    for (int i = 0; i < vi; ++i) {
         int cost, u, v;
-        tie(cost, u, v) = planets[i];
+        tie(cost, u, v) = village[i];
         if (!uni(u, v)) continue ;
         mn += cost;
+        cnt += 1;
+        if (cnt == N - 2)
+            break ;
     }
     cout << mn;
 }
